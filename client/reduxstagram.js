8e679462a2a394f01c4f8656c9726b2ba58ddf20
Anthony Correia <anthony.correia333@yahoo.com>
2017-09-26 12:47:42 -0700

    first commit | set up router

---
+++ client/reduxstagram.js
@@ -0,0 +1,19 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import css from './styles/style.styl';
+
+import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}></Route>
+    </Router>
+)
+
+
+render(<Main/>, document.getElementById('root'));
